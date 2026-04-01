2d57ba08b7db83f5f6fb6909579145339a9f0c7c
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2026-04-01 13:55:31 +0800

    feat: added a prisma orm and migrated the models to the db

---
+++ test.js
@@ -0,0 +1,40 @@
+import { prisma } from './lib/prisma.js';
+async function main() {
+  // Create a new user with a post
+  const user = await prisma.user.create({
+    data: {
+      username: 'JohnFerrancol',
+      password: 'password',
+      files: {
+        create: {
+          filename: 'Hello World',
+          size: 1,
+          path: 'Documents/Project/',
+          mimetype: 'pdf',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      },
+    },
+    include: {
+      files: true,
+    },
+  });
+  console.log('Created user:', user);
+  // Fetch all users with their posts
+  const allUsers = await prisma.user.findMany({
+    include: {
+      files: true,
+    },
+  });
+  console.log('All users:', JSON.stringify(allUsers, null, 2));
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
