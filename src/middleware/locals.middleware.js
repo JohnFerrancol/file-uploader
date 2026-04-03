d58fa3045d0c66ca370d95adeb26c9e17ca2e9bb
JohnFerrancol <88333166+JohnFerrancol@users.noreply.github.com>
2026-04-03 10:54:58 +0800

    fix: fixed navbar and form links for register and log in

--- src/middleware/locals.middleware.js
+++ src/middleware/locals.middleware.js
@@ -1,11 +1,12 @@
 const createLocals = async (req, res, next) => {
   if (req.isAuthenticated()) {
     res.locals.links = [
-      { href: '/', text: 'Home' },
+      { href: '/', text: 'OdinFiles' },
       { href: '/auth/logout', text: 'Log Out' },
     ];
   } else {
     res.locals.links = [
+      { href: '#', text: 'OdinFiles' },
       { href: '/auth/login', text: 'Log In' },
       { href: '/auth/register', text: 'Register' },
     ];
